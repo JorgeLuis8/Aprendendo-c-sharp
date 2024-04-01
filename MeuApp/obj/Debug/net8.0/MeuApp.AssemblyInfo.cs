2e153fb8fca4560e5c36176ad072de7d08c9ed70
Jorge Luis <jorge.luz@ufpi.edu.br>
2024-04-01 16:30:52 -0300

    Tipos primitivos, conversão,operadores aritimeticos e operadores de comparação.

--- MeuApp/obj/Debug/net8.0/MeuApp.AssemblyInfo.cs
+++ MeuApp/obj/Debug/net8.0/MeuApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeuApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88763176b95858214b4a9392b3c4e4b80a9aeecf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be023e2cc772c0bdff2498bf7fec2f92dc4f759d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeuApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeuApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
