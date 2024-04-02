231b72da53e6861cee43450920fd16ca8942efa7
Jorge Luis <jorge.luz@ufpi.edu.br>
2024-04-02 13:47:41 -0300

    funcoes e metodos, mais recursao fatorial

--- MeuApp/Program.cs
+++ MeuApp/Program.cs
@@ -1,63 +1,30 @@
-﻿//int idade; //inicializando com zero
-// int idade = 20; // inicializando com 25
-// //var idade; // errado
+﻿using System;
 
-// //Constantes
-// const int IDADE_MIN = 18;
-// const int IDADE_MAX = 65;
-
-// Numeros inteiros
-// uint idade = 20; // sem sinal
-// short idade = 20; // 16 bits
-// long  idade = 20; // 64 bits
-
-// Numeros reais
-// float idade = 20.5f; // 32 bits
-// double idade = 20.5; // 64 bits
-// decimal idade = 20.5m; // 128 bits
-
-// Caracteres, tem que usar aspas simples.
-// char letra = 'A';
-// var letra1 = 'B';
-
-// // Strings
-// string nome = "Jorginho teste123";
-// var str = "Jorginho teste123";
-
-
-// // Booleanos
-// bool aprovado = true;
-// bool aprovado1 = false;
-
-// // Variáveis implícitas
-// var nome = "Jorginho"; // string
-// var idade = 20; // int
-// var altura = 1.80; // double
-// var aprovado = true; // bool 
-
-// NULL
-// int? idade3 = null;
-// Console.WriteLine(idade3);
-
-
-//conversao implicita
-// float valor = 25.8f;
-// int outro = 25;
-// valor = outro;
-
-// //conversao explicita
-// int inteiro = 100;
-// uint inteiroSemSinal = (uint)inteiro;
-
-//conversao com metodos
-//parse
-// int inteiro = int.Parse("100");
-// Console.WriteLine(inteiro);
-
-//convert
-// int inteiro = Convert.ToInt32("100");
-// inteiro.ToString();
-// Console.WriteLine(inteiro);
-
-//operadores ariticmeticos
+namespace MeuApp
+{
+    class Program{
+        static void Main(string[] args){
+            
+            MeuMetodo();
+            Console.WriteLine(Fat(5));
+            Console.WriteLine(Nome("Jorginho","Silva"));
+        }
+        static void MeuMetodo(){
+            Console.WriteLine("Meu metodo");
+            //output: Meu metodo
+        }
+        static int Fat(int n){
+            var fat = 1;
+            if(n > 1){
+                fat = n * Fat(n-1);
+            }
+            return fat;
+            //output: 120
+        }
+        static string Nome(string nome,string sobrenome){
+            return nome + " " + sobrenome;
+            //output: Jorginho Silva
+        }
+    }
+}
 
